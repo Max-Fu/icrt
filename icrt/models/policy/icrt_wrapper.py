fd5f2fb1305f466b55f7aa49cf1b0109fa832adc
Max-Fu <max.fu.letian@gmail.com>
2024-10-31 15:26:07 -0700

    bug fix for icrt wrapper

--- icrt/models/policy/icrt_wrapper.py
+++ icrt/models/policy/icrt_wrapper.py
@@ -15,18 +15,38 @@ from icrt.util.model_constructor import model_constructor
 from icrt.data.utils import rot_6d_to_euler, quat_to_rot_6d, euler_to_rot_6d
 from icrt.data.utils import convert_delta_action
 
+def undo_vision_transform(obs : torch.Tensor, mean : tuple, std : tuple):
+    """
+    Undo the vision transform applied to the observations.
+    torch tensor has shape T, num_cam, 3, H, W
+    return np.ndarray with shape T, num_cam * H, W, 3 at np.uint8
+    """
+    # undo normalization
+    mean, std = torch.tensor(mean), torch.tensor(std)
+    obs = obs.permute(0, 1, 3, 4, 2)
+    obs = obs * std + mean
+    obs = obs.numpy()
+    obs = np.clip(obs * 255, 0, 255).astype(np.uint8)
+    obs = np.concatenate([obs[:, i] for i in range(obs.shape[1])], axis=1)
+    return obs
+
 class ICRTWrapper(nn.Module):
     def __init__(
         self, 
         train_yaml_path: Union[str, Path],
         checkpoint_path: Union[str, Path],
         vision_encoder_path: Optional[Union[str, Path]] = None,
+        llama_checkpoint_path: Optional[Union[str, Path]] = None,
     ):
         super().__init__()
 
         # loading experiment config 
         args : ExperimentConfig = yaml.load(Path(train_yaml_path).read_text(), Loader=yaml.Loader)
         self.args = args
+
+        if llama_checkpoint_path is not None:
+            args.model_cfg.policy_cfg.llama_ckpt_dir = llama_checkpoint_path
+
         self.device = torch.device(args.device)
 
         # fix the seed for reproducibility
@@ -68,21 +88,31 @@ class ICRTWrapper(nn.Module):
         self.model = model
 
         if self.preprocess is not None:
-            self.preprocess = transforms.Compose(
+            self.preprocess_PIL = transforms.Compose(
                 [t for t in self.preprocess.transforms if not isinstance(t, transforms.ColorJitter)]
+            )
+            self.preprocess_tensor = transforms.Compose(
+                [t for t in self.preprocess.transforms if not isinstance(t, transforms.ToTensor) and not isinstance(t, transforms.ColorJitter)]
             )
         else:
             print("warning: vision transforms are not defined. Using default transforms.")
-            self.preprocess = transforms.Compose([
+            self.preprocess_PIL = transforms.Compose([
                 transforms.Resize(size=248, max_size=None, interpolation=transforms.InterpolationMode.BICUBIC, antialias=True), 
                 transforms.CenterCrop(size=224),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=torch.tensor([0.4850, 0.4560, 0.4060]), std=torch.tensor([0.2290, 0.2240, 0.2250]))
             ])
+            self.preprocess_tensor = transforms.Compose([
+                transforms.Resize(size=248, max_size=None, interpolation=transforms.InterpolationMode.BICUBIC, antialias=True), 
+                transforms.CenterCrop(size=224),
+                transforms.Normalize(mean=torch.tensor([0.4850, 0.4560, 0.4060]), std=torch.tensor([0.2290, 0.2240, 0.2250]))
+            ])
         self.reset()
 
-    def reset(self):
-        self.model.reset(self.model.num_pred_steps)
+    def reset(self, action_exec_horizon = None):
+        if action_exec_horizon is None:
+            action_exec_horizon = self.model.num_pred_steps
+        self.model.reset(action_exec_horizon)
 
     def prompt(
         self,
@@ -108,8 +138,8 @@ class ICRTWrapper(nn.Module):
 
     def prepare_observations(
         self, 
-        side_image: Union[PIL.Image.Image, List[PIL.Image.Image]], 
-        wrist_image : Union[PIL.Image.Image, List[PIL.Image.Image]], 
+        side_image: Union[PIL.Image.Image, List[PIL.Image.Image], np.ndarray], 
+        wrist_image : Union[PIL.Image.Image, List[PIL.Image.Image], np.ndarray], 
         proprio : Union[np.ndarray], 
         action : Optional[np.ndarray] = None
     ):
@@ -122,8 +152,21 @@ class ICRTWrapper(nn.Module):
         """
         if isinstance(side_image, PIL.Image.Image):
             side_image = [side_image]
+            preprocess = self.preprocess_PIL
+        elif isinstance(side_image, np.ndarray):
+            preprocess = self.preprocess_tensor
+            if side_image.dtype == np.uint8:
+                side_image = torch.from_numpy(side_image / 255.0)
+                side_image = side_image.permute(0, 3, 1, 2)
+        else:
+            preprocess = self.preprocess_PIL
+
         if isinstance(wrist_image, PIL.Image.Image):
             wrist_image = [wrist_image]
+        elif isinstance(wrist_image, np.ndarray):
+            if wrist_image.dtype == np.uint8:
+                wrist_image = torch.from_numpy(wrist_image / 255.0)
+                wrist_image = wrist_image.permute(0, 3, 1, 2)
         
         # find the length of the sequence 
         seq_len = len(side_image)
@@ -133,10 +176,10 @@ class ICRTWrapper(nn.Module):
         assert proprio.shape[0] == seq_len, "Length of proprio sequence must match the length of the observation sequence"
         
         # construct input dictionary 
-        side_image = torch.cat([self.preprocess(img)[None] for img in side_image], dim=0)
-        wrist_image = torch.cat([self.preprocess(img)[None] for img in wrist_image], dim=0) 
+        side_image = torch.cat([preprocess(img)[None] for img in side_image], dim=0)
+        wrist_image = torch.cat([preprocess(img)[None] for img in wrist_image], dim=0) 
         # interleave the images 
-        image_vec = torch.stack([side_image, wrist_image], dim=1)[None] # stack along a new axis (seq_len, 2, 3, H, W), then add batch dim
+        image_vec = torch.stack([side_image, wrist_image], dim=1)[None].float() # stack along a new axis (seq_len, 2, 3, H, W), then add batch dim
 
         # process proprio 
         rot = proprio[:, 3:-1]
@@ -152,7 +195,7 @@ class ICRTWrapper(nn.Module):
                 rot = euler_to_rot_6d(rot)
             action = np.concatenate([action[:, :3], rot, action[:, -1:]], axis=-1)
             if self.args.shared_cfg.use_delta_action:
-                action = convert_delta_action(action[None], proprio.numpy())
+                action = convert_delta_action(action[:, None, ...], proprio.numpy().transpose(1, 0, 2)).squeeze()[None, ...]
                 action = torch.tensor(action).float()
             else:
                 action = torch.tensor(action)[None].float()
